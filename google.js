60bc3312dca735df8b6802401baabd749177558b
akanksha-divya <79753104+akanksha-divya@users.noreply.github.com>
2021-07-08 12:08:34 +0530

    Update google.js

--- google.js
+++ google.js
@@ -1,9 +1,11 @@
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   $(".g-signin2").css("display","none");
-   $(".data").css("display","block");
-   $("#pic").attr("src",profile.getImageUrl());
+   $("#name").text(profile.getName());
    $("#email").text(profile.getEmail());
+   $("#pic").attr('src',profile.getImageUrl());
+  $(".data").css("display","block");
+   
 }
 
 
